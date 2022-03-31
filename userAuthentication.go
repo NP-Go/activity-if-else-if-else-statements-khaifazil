81c5d25dc14158007bcb959aa827e7b467e1a4fb
khaifazil <khairal.fazil@gmail.com>
2022-03-31 19:47:46 +0800

    completed activity

--- userAuthentication.go
+++ userAuthentication.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert Code here
+	var userName string
+	fmt.Println("Enter your Username: ")
+	fmt.Scanln(&userName)
+
+	if userName == "Admin" {
+		fmt.Println("Welcome, Admin!")
+	}else if userName == "Robin" || userName == "John" {
+		fmt.Println("Welcome!")
+	}else {
+		fmt.Println("Merry Men")
+	}
 }
